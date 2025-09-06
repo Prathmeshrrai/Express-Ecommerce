925d79c6b1010230597c9672589a3c56a771553b
raiprathmesh <raiprathmesh71@gmail.com>
2025-09-06 20:31:54 +0530

    Added models, config, utils

---
+++ src/models/user.schema.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import AuthRoles from "../utils/authRoles.js"
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: ["true", "Name is required"],
+        maxLength:[50, "Name must be less than 50 chars"]
+    },
+    email:{
+        type:String,
+        required: ["true", "Email is required"],
+    },
+    password:{
+        type:String,
+        required: [true, "Password is required"],
+        minLength: [8, "Password must be atleast 8 chars"],
+        select: false
+    },
+    role:{
+        type:String,
+        enum:Object.values(AuthRoles),
+        default: AuthRoles.USER
+    },
+    forgotPasswordToken:String,
+    forgotPasswordExpiry:Date
+},{timestamps: true})
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next()
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods = {
+    comparePassword: async function(enteredPassword){
+        return await bcrypt.compare(enteredPassword, this.password)
+    }
+}
+
+export default mongoose.model("User", userSchema)
