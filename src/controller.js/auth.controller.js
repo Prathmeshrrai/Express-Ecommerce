8d242a3bae89524cd27928a11b87507c57d72bb5
raiprathmesh <raiprathmesh71@gmail.com>
2025-09-17 23:43:44 +0530

    added customerror

--- src/controller.js/auth.controller.js
+++ src/controller.js/auth.controller.js
@@ -1,4 +1,12 @@
-// import asyncHandler from "../service/asyncHandler";
-// export const signUp = asyncHandler(async(req ,res)) => {
-//     const{name, email, password} = req.body
-// }
+import asyncHandler from "../service/asyncHandler";
+import CustomError from "../utils/CustomError";
+import userSchema from "../models/user.schema";
+
+export const signUp = asyncHandler(async(req ,res) => {
+    const{name, email, password} = req.body
+
+    if(!name || !email || !password){
+        throw new CustomError("please add all fields", 400)
+    }
+
+})
