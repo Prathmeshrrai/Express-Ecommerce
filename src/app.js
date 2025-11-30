052e3f72ad661a134eb657ed9bc44bf71bf1d156
raiprathmesh <raiprathmesh71@gmail.com>
2025-11-30 10:44:55 +0530

    changed routes import

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import router from "./Routes/index.js";
+import router from "./routes/index.js";
 import { sendEmail } from "./utils/mailHelper.js";
 
 
